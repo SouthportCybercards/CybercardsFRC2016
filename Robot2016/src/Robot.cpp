a48d9a0c3149b7b53fb870b6228c7a231fc11bd3
dylan <dylanp6299@gmail.com>
2016-01-22 19:42:17 -0500

    some arm and auto pseudocode

--- Robot2016/src/Robot.cpp
+++ Robot2016/src/Robot.cpp
@@ -32,7 +32,8 @@ private:
 		autoSelected = *((std::string*)chooser->GetSelected());
 		//std::string autoSelected = SmartDashboard::GetString("Auto Selector", autoNameDefault);
 		std::cout << "Auto selected: " << autoSelected << std::endl;
-
+		Encoder *enc;
+		enc = new Encoder(/*DIO port 0, 1*/0, 1, false, Encoder::EncodingType::k4X);
 		if(autoSelected == autoNameCustom){
 			//Custom Auto goes here
 		} else {
@@ -42,7 +43,12 @@ private:
 			 * Specify defences, where etc
 			 * Calibrate sensors
 			 */
-
+/*Autoinit: getEncoderValue (on the arm)
+ * calculate a 75 degree angle based on the arm starting at 90 degrees
+ * calibrate the color sensors
+ *
+ *
+ */
 
 
 			//Default Auto goes here
@@ -63,6 +69,21 @@ private:
 			 * 		ReadOtherSensors()
 			 * 		DriveMotors()
 			 * 		LiftGate() ...?
+			 *
+			 * 		read arm encoder
+			 * 		drive arm at full speed to 75 degrees
+			 * 		drive arm slowly to zero switch
+			 * 		drive arm up to 20 degrees
+			 * 		check sensors
+			 *
+			 * 		intake:
+			 * 		read controls
+			 * 			make controls for intake cancel, shot cancel, shot trigger, intake trigger
+			 * 			read limit switches--all wired to one io port
+			 * 			drive (direction*deadzone*speed)
+			 * 			do debouncing
+			 * 			shot timer to turn off roller automatically
+			 * 			dont intake if limit switches are triggered
 			 */
 
 
@@ -90,6 +111,10 @@ private:
 		 * 			drive()
 		 * DRIVE[DriveHook()
 		 * DRIVE[DriveBallStuff()
+		 *
+		 * read controls
+		 * move arm to set points (0, 20, 90, 110 degrees)
+		 * "deal with the gas cylinder"
 		 */
 	}
 
