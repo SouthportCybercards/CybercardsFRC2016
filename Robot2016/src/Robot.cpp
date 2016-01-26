6e2a827b12b67ced705940b95112f1a2b4ae3386
dylan <dylanp6299@gmail.com>
2016-01-25 19:33:05 -0500

    Fixed everything gg (i.e. issue 3 and more)

--- Robot2016/src/Robot.cpp
+++ Robot2016/src/Robot.cpp
@@ -2,12 +2,30 @@
 
 class Robot: public IterativeRobot
 {
+public:
+	Robot() :
+		robotDrive(9, 8, 7, 6),	// these must be initialized in the same order
+		leftStick(0),
+		rightStick(1),
+		lw(NULL),
+		chooser()
+
+	{
+		robotDrive.SetExpiration(0.1);
+		robotDrive.SetInvertedMotor(robotDrive.kFrontLeftMotor,false);
+		robotDrive.SetInvertedMotor(robotDrive.kFrontRightMotor,false);
+		robotDrive.SetInvertedMotor(robotDrive.kRearLeftMotor,false);
+		robotDrive.SetInvertedMotor(robotDrive.kRearRightMotor,false);
+	}
 private:
+	RobotDrive robotDrive;
+	Joystick leftStick, rightStick;
 	LiveWindow *lw = LiveWindow::GetInstance();
 	SendableChooser *chooser;
 	const std::string autoNameDefault = "Default";
 	const std::string autoNameCustom = "My Auto";
 	std::string autoSelected;
+	Joystick leftStick, rightStick;
 
 	void RobotInit()
 	{
@@ -15,14 +33,13 @@ private:
 		chooser->AddDefault(autoNameDefault, (void*)&autoNameDefault);
 		chooser->AddObject(autoNameCustom, (void*)&autoNameCustom);
 		SmartDashboard::PutData("Auto Modes", chooser);
+
 	}
 
 
 	/**
 	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
-	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
-	 * Dashboard, remove all of the chooser code and uncomment the GetString line to get the auto name from the text box
-	 * below the Gyro
+	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard.
 	 *
 	 * You can add additional auto modes by adding additional comparisons to the if-else structure below with additional strings.
 	 * If using the SendableChooser make sure to add them to the chooser code above as well.
@@ -43,14 +60,12 @@ private:
 			 * Specify defences, where etc
 			 * Calibrate sensors
 			 */
-/*Autoinit: getEncoderValue (on the arm)
- * calculate a 75 degree angle based on the arm starting at 90 degrees
- * calibrate the color sensors
- *
- *
- */
-
-
+			/*Autoinit: getEncoderValue (on the arm)
+			 * calculate a 75 degree angle based on the arm starting at 90 degrees
+			 * calibrate the color sensors
+			 *
+			 *
+			 */
 			//Default Auto goes here
 		}
 	}
@@ -61,8 +76,6 @@ private:
 			//Custom Auto goes here
 		} else {
 			//Default Auto goes here
-
-
 			/*
 			 * Auton()
 			 * 		ColorSense()
@@ -85,8 +98,6 @@ private:
 			 * 			shot timer to turn off roller automatically
 			 * 			dont intake if limit switches are triggered
 			 */
-
-
 		}
 	}
 
@@ -97,6 +108,19 @@ private:
 
 	void TeleopPeriodic()
 	{
+		//Local declarations
+		float driveThreshold = 0.005;
+
+		//Get the y-axis of the joystick
+
+		float yAxis1Raw = 1 * leftStick.GetY();
+		float yAxis2Raw = 1 * rightStick.GetY();
+
+		//Drive the drive motors when any input is within  -driveThreshold of 0.0
+		//NOTE - currently this doesn't scale up the input from 0.0 after the deadband region -- it just uses the raw value.
+		float yAxis1 = DeadZone(yAxis1Raw, driveThreshold, 0.0f);
+		float yAxis2 = DeadZone(yAxis2Raw, driveThreshold, 0.0f);
+		robotDrive.TankDrive(-yAxis1,-yAxis2); 	// drive
 		/*{Time()
 		 * INPUT[Camera()
 		 * 			SendToBase()
@@ -122,6 +146,16 @@ private:
 	{
 		lw->Run();
 	}
+	float DeadZone(float value, float margin, float target){
+		//deadzone code, will return og value if > or < drivethreshold
+		if(value != target){
+			if(value > target + margin || value < target - margin){
+				return value;
+			}
+		}
+		return target;
+	}
+
 };
 
 START_ROBOT_CLASS(Robot)
